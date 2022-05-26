8e723f42e6a0f3967f29c986af8ded15154b7db4
jiZky <mosalah2018##>
2022-05-26 09:59:52 +0100

    First commit

---
+++ src/main/java/com/vermeg/productivity/forecastmanager/ForecastManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vermeg.productivity.forecastmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastManagerApplication.class, args);
+	}
+
+}
